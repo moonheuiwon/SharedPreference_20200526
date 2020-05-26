c97abdb3a1d7500d14e43def2188ac6caf3084da
moonheuiwon <wonheuimoon@gmail.com>
2020-05-26 12:38:43 +0900

    회원가입 화면 이동하기

--- app/src/main/java/com/example/sharedpreference_20200526/MainActivity.java
+++ app/src/main/java/com/example/sharedpreference_20200526/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.sharedpreference_20200526;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
@@ -25,6 +26,14 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(mContext, SignUpActivity.class);
+                startActivity(myIntent);
+            }
+        });
 
         binding.idSaveCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
